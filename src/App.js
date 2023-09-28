de8bb5ae6a0898bb83a06072d8624c512a8c89ce
AnaSeiculescu <anaseiculescu@gmail.com>
2023-09-28 10:58:28 +0300

    we are using the dark theme of Material UI, and we create the Navbar to show the Logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
+import Navbar from './Components/Navbar';
+import Result from "./Component/Result";
+import SliderSelect from "./Components/SliderSelect";
+import TenureSelect from "./Components/TenureSelect";
+
 
 function App() {
-  return <div className="App">Hello everyone!</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
