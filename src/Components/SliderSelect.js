bf52e39c4c82e6a1dc33a0de9e070861fd7e5493
AnaSeiculescu <anaseiculescu@gmail.com>
2023-10-04 18:33:11 +0300

    creating the slider component and start building the SliderSelect Component

--- src/Components/SliderSelect.js
+++ src/Components/SliderSelect.js
@@ -1,7 +1,40 @@
 import React from "react";
+import SliderComponent from "./Common/SliderComponent";
 
 const SliderSelect = () => {
-    return <div>SliderSelect</div>
+    return (
+        <div>
+            <SliderComponent
+                defaultValue={20}
+                min={1000}
+                max={10000}
+                onChange={(e, value) => console.log(value)}
+                step={100}
+                amount={3000}
+                label={'Home Value'}
+            />
+
+            <SliderComponent
+                defaultValue={20}
+                min={1000}
+                max={10000}
+                onChange={(e, value) => console.log(value)}
+                step={100}
+                amount={3000}
+                label={'Down Payment'}
+            />
+
+            <SliderComponent
+                defaultValue={20}
+                min={1000}
+                max={10000}
+                onChange={(e, value) => console.log(value)}
+                step={100}
+                amount={3000}
+                label={'Loan Amount'}
+            />
+        </div>
+    )
 }
 
 export default SliderSelect;
